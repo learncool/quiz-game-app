9462934a642dc01d590058fd6e014decae018714
prabhat <prabhat.04011998@gmail.com>
2019-07-28 00:11:07 +0530

    quiz ui changed to black

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -9,7 +9,7 @@ import '../App.css';
 
 class Quiz extends Component {
 
-	constructor(props) {
+  constructor(props) {
         super(props);
         this.state = {
             
@@ -19,24 +19,26 @@ class Quiz extends Component {
 
 
   render(){
-  	return(
-  		<div>
-			  <button class="ui secondary button" style={{margin:'10px'}}>Start Quiz</button>
+    return(
+      <div style={{background:'#222',color:'white',border:'1px solid grey'}}>
 
-  		<div className="row" style={{backgroundColor:'  #a6acaf',height:'200px',textAlign:'Left',padding:'10px',borderRadius:'20px',background:'linear-gradient(to right, #a6acaf,#f2f3f4 40%,#f2f3f4 100%)',margin:'20px'}}>
-  		 	<p><strong>Q)</strong> : Question is</p>
-  		 	<div >
-			  <div  style={{height:'30px',padding:'6px',textAlign:'center',margin:'10px',borderRadius:'20px',background:'linear-gradient(to right,  #cacfd2 ,rgb(24,243,244) 40%,rgb(100,100,179) 80%)'}}>Hello</div>
-			  <div  style={{height:'30px',padding:'6px',textAlign:'center',margin:'10px',borderRadius:'20px',background:'linear-gradient(to right,  #cacfd2 ,rgb(24,243,244) 40%,rgb(100,100,179) 80%)'}}>Hello</div>
-			  <div  style={{height:'30px',padding:'6px',textAlign:'center',margin:'10px',borderRadius:'20px',background:'linear-gradient(to right,  #cacfd2 ,rgb(24,243,244) 40%,rgb(100,100,179) 80%)'}}>Hello</div>
-			  <div  style={{height:'30px',padding:'6px',textAlign:'center',margin:'10px',borderRadius:'20px',background:'linear-gradient(to right,  #cacfd2 ,rgb(24,243,244) 40%,rgb(100,100,179) 80%)'}}>Hello</div>
-			</div>
+        <button class="ui secondary button" style={{margin:'10px'}}>Start Quiz</button>
+
+      <div className="row" style={{textAlign:'center',background:'#222'}}>
+        <p>Question 1 of 10</p>
+        <strong><h2>Which one is not a sorting algorithm?</h2></strong>
+        <div >
+        <div  style={{padding:'10px',border:'1px solid',textAlign:'center',margin:'10px',background:'#222'}}>Quick Sort</div>
+        <div  style={{padding:'10px',border:'1px solid',textAlign:'center',margin:'10px',background:'#222'}}>Merge Sort</div>
+        <div  style={{padding:'10px',border:'1px solid',textAlign:'center',margin:'10px',background:'#222'}}>Heap Sort</div>
+        <div  style={{padding:'10px',border:'1px solid',textAlign:'center',margin:'10px',background:'#222'}}>Hash Sort</div>
+      </div>
 
          
-		</div>
-		</div>
+    </div>
+    </div>
 
-  		)
+      )
   }
 }
 
