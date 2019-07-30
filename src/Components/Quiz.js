99e18a0b9dea33ad1989622eee524f82ee6cd2d7
prabhat <prabhat.04011998@gmail.com>
2019-07-31 02:47:26 +0530

    cursor:pointer

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -20,7 +20,6 @@ changeColor(){
 	el.classList.add("greenButton");
 
 	console.log("dkfjdl",document.getElementById('2'));
-    this.setState({green: !this.state.green})
 }
 
 
@@ -34,10 +33,10 @@ changeColor(){
 			<button class="ui secondary button" style={{margin:'10px'}}>{this.props.part}</button>	  		
 	      	<div style={{backgroundColor:'#aeb6bf',padding:'10px',margin:'10px'}} onClick={this.changeColor.bind(this)}>
 	      	<p>Which of the following is not a data-structure?</p>
-	      		<div id='1' className="whiteButton" style={{borderRadius:'0 20px 20px 0',paddingTop:'6px',marginTop:'10px',height:'30px'}}>Heap<i style={{float:'right'}} aria-hidden="true" class="chevron circle right icon"></i></div>
-	      		<div id='2' className="whiteButton" style={{borderRadius:'0 20px 20px 0',paddingTop:'6px',marginTop:'10px',height:'30px'}}>Hash<i style={{float:'right'}} aria-hidden="true" class="chevron circle right icon"></i></div>
-	      		<div id='3' className="whiteButton" style={{borderRadius:'0 20px 20px 0',paddingTop:'6px',marginTop:'10px',height:'30px'}}>BST<i style={{float:'right'}} aria-hidden="true" class="chevron circle right icon"></i></div>
-	      		<div id='4' className="whiteButton" style={{borderRadius:'0 20px 20px 0',paddingTop:'6px',marginTop:'10px',height:'30px'}}>Queue<i style={{float:'right'}} aria-hidden="true" class="chevron circle right icon"></i></div>
+	      		<div id='1' className="whiteButton" style={{borderRadius:'0 20px 20px 0',cursor:'pointer',paddingTop:'6px',marginTop:'10px',height:'30px'}}>Heap<i style={{float:'right'}} aria-hidden="true" class="chevron circle right icon"></i></div>
+	      		<div id='2' className="whiteButton" style={{borderRadius:'0 20px 20px 0',cursor:'pointer',paddingTop:'6px',marginTop:'10px',height:'30px'}}>Hash<i style={{float:'right'}} aria-hidden="true" class="chevron circle right icon"></i></div>
+	      		<div id='3' className="whiteButton" style={{borderRadius:'0 20px 20px 0',cursor:'pointer',paddingTop:'6px',marginTop:'10px',height:'30px'}}>BST<i style={{float:'right'}} aria-hidden="true" class="chevron circle right icon"></i></div>
+	      		<div id='4' className="whiteButton" style={{borderRadius:'0 20px 20px 0',cursor:'pointer',paddingTop:'6px',marginTop:'10px',height:'30px'}}>Queue<i style={{float:'right'}} aria-hidden="true" class="chevron circle right icon"></i></div>
 	      	
 	      	</div>
 
